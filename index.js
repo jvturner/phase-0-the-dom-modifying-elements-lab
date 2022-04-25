8bb517abd6148df2708acd751c378e80fec487ed
Jamond Turner <jamondturner@yahoo.com>
2022-04-24 21:16:45 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your code here!
+let element = document.getElementById("main");
+console.log(element);
+element.remove(element);
+
+
+
+
+
+let newHeader = document.createElement('h1');
+console.log(newHeader);
+document.body.appendChild(newHeader);
+newHeader.setAttribute('victory');
+newHeader.innerText("Jamond is the champion");
+
+// 
+// document.getElementsByTagName('h1');
+// intro.setAttribute(id,"victory");
+
+
+
+
+
+
+// then use element.remove()to remove the elemen
+// ;
+
+// document.getElementsByTagName('h1');
+
+
+
+
+
+
+
+
+
+
+
+
+// let newHeader = document.createElement('h1');
+
+// newHeader.id
+
+
+
